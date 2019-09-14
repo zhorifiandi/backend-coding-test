3319d7626eb6780aa4cad1d2e6769fd3a454bbec
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2019-09-14 09:11:57 +0700

    implement winston

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const sqlite3 = require("sqlite3").verbose();
+const winston = require("winston");
 const buildSchemas = require("./src/schemas");
 const app = require("./src/app");
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+  ],
+});
 
 const db = new sqlite3.Database(":memory:");
 const port = 8010;
@@ -8,5 +15,5 @@ const port = 8010;
 db.serialize(() => {
   buildSchemas(db);
 
-  app(db).listen(port, () => console.log(`App started and listening on port ${port}`));
+  app(db).listen(port, () => logger.info(`App started and listening on port ${port}`));
 });
