cd1c656f63e28d3af2ae016e6c8345cdcf68c3a4
Ari Pratama Zhorifiandi <arizho16@gmail.com>
2019-09-16 19:55:56 +0700

    init readme as /docs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const basicAuth = require("express-basic-auth");
 const config = require("config");
+const fs = require("fs");
+const markdown = require( "markdown" ).markdown;
 
 const userGranted = {};
 userGranted[config.get("BasicAuth.username")] = config.get("BasicAuth.password");
 
 const app = express();
-app.use(basicAuth({
-  users: userGranted,
-}));
+// app.use(basicAuth({
+//   users: userGranted,
+// }));
 
 const bodyParser = require("body-parser");
 const ridesController = require("./controller/rides");
@@ -17,6 +19,11 @@ const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
   app.get("/health", (req, res) => res.send("Healthy"));
+  app.get("/docs", (req, res) => {
+    fs.readFile('./docs/Readme.md', 'utf8', (err, data) => {
+        res.send(markdown.toHTML(data))
+    });
+  });
   app.get("/rides", (req, res) => ridesController.GetRides(db, req, res));
   app.get("/rides/:id", (req, res) => ridesController.GetRideById(db, req, res));
   app.post("/rides", jsonParser, (req, res) => ridesController.InsertRide(db, req, res));
